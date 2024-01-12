4e536e5ddb1014e290437fa971ab552fb8c1828e
dopnetindo <dopnetindo@gmail.com>
2024-01-13 01:59:53 +0700

    new

--- main_oop.py
+++
@@ -1,7 +0,0 @@
-from controller import ConfigController
-
-
-controller = ConfigController(
-    "./templates/ngfw_default.yml", "./templates/ngfw_default.j2", "devices.json"
-)
-controller.configure_routers()
